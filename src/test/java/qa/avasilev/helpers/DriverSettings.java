a1a38252ca02a66a5afed53655832cf1a59200b3
Aleksei_Vasilev <aleksei.vasilev@iii.com>
2022-07-08 23:28:40 +0300

    build.gradle updated

--- src/test/java/qa/avasilev/helpers/DriverSettings.java
+++ src/test/java/qa/avasilev/helpers/DriverSettings.java
@@ -15,7 +15,6 @@ public class DriverSettings {
         Configuration.browserVersion = Project.config.browserVersion();
         Configuration.browserSize = Project.config.browserSize();
         Configuration.baseUrl = Project.config.webUrl();
-        //Configuration.headless = true;
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         ChromeOptions chromeOptions = new ChromeOptions();
